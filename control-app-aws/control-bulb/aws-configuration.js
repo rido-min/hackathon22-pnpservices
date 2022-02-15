a13aef019c745c03d36a24a237864fe0de1cb71f
ridomin <ridomin@live.com>
2022-02-14 22:53:28 -0800

    add aws contro;1

---
+++ control-app-aws/control-bulb/aws-configuration.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2015-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * NOTE: You must set the following string constants prior to running this
+ * example application.
+ */
+var awsConfiguration = {
+   poolId: 'us-west-1:d96006ab-1cf6-4ca5-a890-edeba4d05549', // 'YourCognitoIdentityPoolId'
+   host: 'a38jrw6jte2l2x-ats.iot.us-west-1.amazonaws.com', // 'YourAWSIoTEndpoint', e.g. 'prefix.iot.us-east-1.amazonaws.com'
+   region: 'us-west-1' // 'YourAwsRegion', e.g. 'us-east-1'
+};
+module.exports = awsConfiguration;
+
